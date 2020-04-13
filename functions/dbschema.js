2e3e4a2799b99a2f63a117ed3e913a627e68f062
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-04-13 17:10:45 -0300

    feat: Add sign up route

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  posts: [
+    {
+      shortUser: "user",
+      bodyMessage: "Post body here",
+      createdAt: "2020-04-13T17:00:00.018Z",
+      likeCount: 1,
+      commentCount: 2,
+    },
+  ],
+};
