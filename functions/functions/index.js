40d434194fbe932dc28936499c87dd3cd9b7b7bc
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-04-13 13:37:31 -0300

    feat: Add getPosts and createPost endpoint

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,8 +1,49 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+// Get all Posts
+exports.getPosts = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("posts")
+    .get()
+    .then((data) => {
+      let posts = [];
+      data.forEach((dataItem) => {
+        posts.push(dataItem.data());
+      });
+      return res.status(201).json(posts);
+    })
+    .catch((err) => console.error(err));
+});
+
+// Create a new Post
+exports.createPost = functions.https.onRequest((req, res) => {
+  const { bodyMessage, shortUser } = req.body;
+  let createdAt = admin.firestore.Timestamp.fromDate(new Date());
+  const newPost = {
+    shortUser,
+    bodyMessage,
+    createdAt,
+  };
+
+  admin
+    .firestore()
+    .collection("posts")
+    .add(newPost)
+    .then((data) => {
+      res.json({ message: `Document ${data.id} created successfully!` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Something went wrong!" });
+      console.log(err);
+    });
+});
