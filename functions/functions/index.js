188e6c73a8e20592e8d5c403fbdc8a7f670e5424
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-04-15 20:07:37 -0300

    feat: Add more functions

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -113,3 +113,55 @@ exports.createNotificationOnComment = functions.firestore
         return;
       });
   });
+
+exports.onUserImageChange = functions.firestore
+  .document("/users/{userId}")
+  .onUpdate((change) => {
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log("image has changed");
+      let batch = db.batch();
+
+      return db
+        .collection("posts")
+        .where("shortName", "==", change.before.data().shortName)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const post = db.doc(`/posts/${doc.id}`);
+            batch.update(post, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    }
+  });
+
+exports.onPostDelete = functions.firestore
+  .document("/posts/{postId}")
+  .onDelete((snapshot, context) => {
+    const postId = context.params.postId;
+    const batch = db.batch();
+
+    return db
+      .collection("comments")
+      .where("postId", "==", postId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db.collection("likes").where("postId", "==", postId);
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db.collection("notifications").where("postId", "==", postId);
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => console.error(err));
+  });
