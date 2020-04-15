90a8d6f1f93ea3592d3f300518f5fddbf246659b
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-04-14 22:44:29 -0300

    feat: Add notifications on like/comment

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,5 +1,5 @@
 const functions = require("firebase-functions");
-
+const { db } = require("./utils/index");
 const app = require("express")();
 
 const { BAuth } = require("./utils/auth");
@@ -49,3 +49,69 @@ app.post("/user/image", BAuth, uploadImage);
 app.post("/user", BAuth, addUserBio);
 
 exports.api = functions.https.onRequest(app);
+
+exports.createNotificationOnLike = functions.firestore
+  .document("likes/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().shortName,
+            sender: snapshot.data().shortName,
+            type: "like",
+            read: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.deleteNotificationOnUnLike = functions.firestore
+  .document("likes/{id}")
+  .onDelete((snapshot) => {
+    db.doc(`/notifications/${snapshot.id}`)
+      .delete()
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
+
+exports.createNotificationOnComment = functions.firestore
+  .document("comments/{id}")
+  .onCreate((snapshot) => {
+    db.doc(`posts/${snapshot.data().postId}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return db.doc(`/notifications/${snapshot.id}`).set({
+            createdAt: new Date().toISOString(),
+            recipient: doc.data().shortName,
+            sender: snapshot.data().shortName,
+            type: "comment",
+            read: false,
+            postId: doc.id,
+          });
+        }
+      })
+      .then(() => {
+        return;
+      })
+      .catch((err) => {
+        console.error(err);
+        return;
+      });
+  });
