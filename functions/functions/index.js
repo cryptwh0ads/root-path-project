8c2c56be5116b5ef3950e3f31ab5208df49655cb
Vinicius Cruz <wh0adscrypt@gmail.com>
2020-04-15 20:28:48 -0300

    fix: Fixed an error in onPostDelete

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -132,7 +132,7 @@ exports.onUserImageChange = functions.firestore
           });
           return batch.commit();
         });
-    }
+    } else return true;
   });
 
 exports.onPostDelete = functions.firestore
@@ -149,13 +149,16 @@ exports.onPostDelete = functions.firestore
         data.forEach((doc) => {
           batch.delete(db.doc(`/comments/${doc.id}`));
         });
-        return db.collection("likes").where("postId", "==", postId);
+        return db.collection("likes").where("postId", "==", postId).get();
       })
       .then((data) => {
         data.forEach((doc) => {
           batch.delete(db.doc(`/likes/${doc.id}`));
         });
-        return db.collection("notifications").where("postId", "==", postId);
+        return db
+          .collection("notifications")
+          .where("postId", "==", postId)
+          .get();
       })
       .then((data) => {
         data.forEach((doc) => {
